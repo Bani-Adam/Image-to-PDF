5c7cd73c693986365b47ec6c53fd786357daef41
Bani-Adam <042528546@ecampus.ut.ac.id>
2023-09-18 23:22:51 +0700

    Update

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
       href="https://github.com/Bani-Adam/Image-to-PDF"
       className="flex flex-row justify-center items-center lg:space-x-4 space-x-2"
     >
-      <p className="lg:text-lg text-xs">&copy; ADAM DEV.</p>
+      <p className="lg:text-lg text-xs">&copy; ADAM DEV. 2023</p>
       <div className="lg:w-6 w-4 lg:h-6 h-4 relative">
         <Image src="/github-logo.png" alt="github-logo" fill />
       </div>
